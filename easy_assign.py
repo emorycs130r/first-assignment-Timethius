6562d170c5420215d42dad2592515da2aae935d0
Timethius <78006393+Timethius@users.noreply.github.com>
2021-02-11 23:07:30 -0500

    Add files via upload

--- easy_assign.py
+++ easy_assign.py
@@ -16,19 +16,34 @@ DO NOT EDIT THE FUNCTION NAMES.
 
 def append_two_strings(string_1, string_2):
 
-    pass
+    #print(f"String 1 Is: {string_1}")
+    #print(f"String 2 Is: {string_2}")
+    sum = input(string_1 + string_2)
+    return sum
+    
 
 
 def append_character(string_1, char_1):
 
-    pass
+    #print(f"String 1 Is: {string_1}")
+    #print(f"Character 1 Is: {char_1}")
+    sum = input(string_1 + char_1)
+    return sum
+    
 
 
 def append_num_to_string(string_1, num_1):
 
-    pass
+    #print(f"String 1 Is: {string_1}")
+    #print(f"Number 1 Is: {num_1}")
+    sum = input(string_1 + num_1)
+    return sum
 
 
 if __name__ == "__main__":
     
-    pass
+    string_1 = str(input("Enter 1st Word: "))
+    string_2 = str(input("Enter 2nd Word: "))
+    char_1 = str(input("Enter Character: "))
+    num_1 = str(input("Enter Number: "))
+    result = append_two_strings(string_1, string_2), append_character(string_1, char_1), append_num_to_string(string_1, num_1)
